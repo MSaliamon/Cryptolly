cbcd965559471446dd75764b244d29d1e30b2b98
MSaliamon <maksimsalamon181@gmail.com>
2025-03-29 11:30:17 +0200

    section4-finish

--- src/components/main/section4/Section4.jsx
+++ src/components/main/section4/Section4.jsx
@@ -1,8 +1,18 @@
 import './Section4.scss'
-
+import ArrowLeft from '../../../assets/homepage/svg/ArrowLeft.svg'
+import ArrowRight from '../../../assets/homepage/svg/ArrowRight.svg'
 function Section4() {
   return (
-    <div>Section4</div>
+    <section className="section4">
+        <img src={ArrowLeft} alt="ArrowLeft" className="section4-img" />
+        <div className="section4-slider">
+            <div className="section4-slider__slide">
+                <p className="section4-slider__slide-text1">Forbes</p>
+                <p className="section4-slider__slide-text2">“Derivative exchange America-based Cryptolly believes they will continue to grow in 2020.”</p>
+            </div>
+        </div>
+        <img src={ArrowRight} alt="ArrowRight" className="section4-img" />
+    </section>
   )
 }
 
